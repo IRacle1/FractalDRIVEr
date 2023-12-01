44b4aced41378d4af9e9daf1c088da821f6ed5da
IRacle <zornik2006@yandex.ru>
2023-12-01 18:41:15 +0300

    debug

--- FractalDRIVEr/Shader.cs
+++ FractalDRIVEr/Shader.cs
@@ -25,22 +25,26 @@ namespace FractalDRIVEr
 
             GL.CompileShader(FragmentShader);
 
+#if DEBUG
             GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out int successF);
 
             Console.WriteLine(successF);
 
             Console.WriteLine(GL.GetShaderInfoLog(FragmentShader));
+#endif
 
             VertexShader = GL.CreateShader(ShaderType.VertexShader);
             GL.ShaderSource(VertexShader, vertexShaderSource);
 
             GL.CompileShader(VertexShader);
 
+#if DEBUG
             GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int successV);
             
             Console.WriteLine(successV);
 
             Console.WriteLine(GL.GetShaderInfoLog(VertexShader));
+#endif
 
             Handle = GL.CreateProgram();
 
@@ -49,11 +53,13 @@ namespace FractalDRIVEr
 
             GL.LinkProgram(Handle);
 
+#if DEBUG
             GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out int successProgmar);
 
             Console.WriteLine(successProgmar);
 
             Console.WriteLine(GL.GetProgramInfoLog(Handle));
+#endif
 
             GL.DetachShader(Handle, VertexShader);
             GL.DetachShader(Handle, FragmentShader);
